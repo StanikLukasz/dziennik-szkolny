46cb4d5ff71ea76b1ed283b3b0d790bac2450132
Wojtek <wojtex1221@gmail.com>
2020-04-18 18:58:40 +0200

    Added sessions to the websites. Main page no longer contains data in the url bar. Added "logout" page, that deletes session data and redirects to login page. Added "dataProblem" page, that is shown whenever there is a problem with session data.

--- frontend/dziennik.py
+++ frontend/dziennik.py
@@ -1,14 +1,19 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "pFosAGEabuabfaSDA"
 
 
 @app.route("/")  # jeśli nie jesteś zalogowany -> przekieruj na stronę logowania, w przeciwnym wypadku -> mainPage
 def startPage():
-    # if loggedIn:
-    #    go to mainPage()
-    # else:
-    return redirect(url_for("loginPage"))
+    if "status" in session:
+        if session["status"] == "loggedIn":
+            return redirect(url_for("mainPage"))
+        else:
+            # TODO tutaj zwróć stornę "You have been logged out"
+            return redirect(url_for("loginPage"))
+    else:
+        return redirect(url_for("loginPage"))
 
 
 
@@ -18,16 +23,65 @@ def loginPage():
         formEmail = request.form["email"]
         formPassword = request.form["password"]
 
+        if len(formEmail) < 1 or len(formPassword) < 1:  # zabezpieczenie przed błędnym wprowadzaniem
+            return render_template("loginPage.html")
+
         # sprawdź tutaj czy takie konto figuruje w bazie danych, jeśli nie - zwróć error
-        return redirect(url_for("mainPage", name=formEmail, password=formPassword))
+
+        # dane sesji:
+        session["email"] = formEmail
+        session["password"] = formPassword  # to idzie później w kosz, nie chcemy hasła trzymać w sesji, używamy go jedynie do autentykacji na początku
+        session["status"] = "loggedIn"
+
+        return redirect(url_for("mainPage"))
     else:
-        return render_template("loginPage.html")
+        if "status" in session:
+            if session["status"] == "loggedIn":
+                return redirect(url_for("mainPage"))
+            else:
+                # TODO tutaj zwróć stornę "You have been logged out"
+                return render_template("loginPage.html")
+        else:
+            return render_template("loginPage.html")
 
 
 
-@app.route("/main-<name>-<password>")  # rozumiem to jako pierwsza strona, jaką już zalogowany użytkownik zobaczy, czyli tam gdzie będzie lista funkcji i np. tablica
-def mainPage(name, password):
-    return render_template("boardPage.html", email=name, password=password)
+@app.route("/main")  # rozumiem to jako pierwsza strona, jaką już zalogowany użytkownik zobaczy, czyli tam gdzie będzie lista funkcji i np. tablica
+def mainPage():
+    if "status" in session:
+        if "email" in session:  # tu jest poprawna sesja
+            email = session["email"]
+            return render_template("boardPage.html", email=email)
+
+
+
+        else:
+            return redirect(url_for("loginPage"))
+    else:
+        return redirect(url_for("loginPage"))
+
+
+
+@app.route("/logout", methods=["POST", "GET"])
+def logoutPage():
+    if request.method == "GET":
+        try:
+            session.clear()
+            return render_template("logoutPage.html")
+        except:
+            return render_template(url_for("dataProblem"))
+    else:
+        return redirect(url_for("loginPage"))
+
+
+@app.route("/dataProblem", methods=["POST", "GET"])  # kiedy np. braknie gdzieś twojego maila w sesji, bądź czas sesji się skończy
+def dataProblem():
+    if request.method == "GET":
+        session.clear()
+        return render_template("dataProblem.html")
+    else:
+        return redirect(url_for("loginPage"))
+
 
 
 @app.route("/<wrongPageURL>", methods=["POST", "GET"])  # "pretty self explanatory", zastąpienie wadliwego adresu naszą stronką, aby nie było basic error screen'a
@@ -35,7 +89,7 @@ def wrongPage(wrongPageURL):
     if request.method == "GET":
         return render_template("wrongPage.html", url=wrongPageURL)
     else:
-        return redirect(url_for("startPage")) # this is for "return home" button
+        return redirect(url_for("startPage"))  # this is for "return home" button
 
 
 
