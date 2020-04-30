ac3869eb908150d10d4a687171ca57e11bb3d9b8
Wojtek <wojtex1221@gmail.com>
2020-05-01 00:02:48 +0200

    Added permanent sessions to the website (for now it's ON by default, later we might change it to user choice when logging in).

--- frontend/dziennik.py
+++ frontend/dziennik.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template, request, session
 from datetime import timedelta
 
 app = Flask(__name__)
-app.secret_key = "pFosAGEabuabfaSDA"
+app.secret_key = "pFosAGEabuabfaSDAd"
 app.permanent_session_lifetime = timedelta(minutes=15)
 
 
